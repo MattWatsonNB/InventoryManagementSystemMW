0078d0db61d1c31a38b2a2adde4144a75761bc8e
unknown <mwatson@PC-CZC451005D.netbuilder.private>
2015-06-23 17:34:35 +0100

    2nd

--- src/mwatsonIMS/IMS.java
+++ src/mwatsonIMS/IMS.java
@@ -1,41 +1,52 @@
 package mwatsonIMS;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class IMS {
 
-	int[] PI;
-	String[] PN;
-	
+	ArrayList<Product> allproducts = new ArrayList<Product>();
+	Product p1 = new Product();
 	
 	public IMS() { 
-		PI = getProductID();
-		PN = getProductName();
+			
+		allproducts.add(new Product(0, 10, "gnome"));
 		
-		for (int i = 0; i < 4; i++){
-		System.out.println(PI[i] + " " + PN[i] );
+		
+		/*
+		for (int i = 0; i < 2; i ++ ){
+		System.out.println("These are all contained in the array " + allproducts.get(i).getProductQty());
+		}
+		*/
+		
+		for (Product p : allproducts) {
+			System.out.println(p.toString());
 		}
 		
-		ArrayList<Product> allproducts = new ArrayList<Product>();
-		Product p = new Product();
-		p.setProductName("Hello");
-		p.setProductID(2010);
-		p.setProductQty(12);
-		allproducts.add(p);
-		System.out.println(allproducts.get(0).getProductName() + ", " + allproducts.get(0).getProductID() + " & " + allproducts.get(0).getProductQty());
+		inputmethod();
+		
+		for (Product p : allproducts) {
+			System.out.println(p.toString());
+		}
+	}	
+		
+	public void inputmethod() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("What is the number?");
+		int Qty = scan.nextInt();
+		p1.setProductID(Qty); 
+		allproducts.add(new Product(p1.getProductID()));
+		
+		for (Product p : allproducts) {
+			System.out.println(p.toString());
+		}
+		
+		System.out.println("?");
+		int Qty1 = scan.nextInt();
+		p1.setProductID(Qty1); 
+		allproducts.add(new Product(p1.getProductID()));
 		
 		
 	}
-	
-	public int[] getProductID() {
-		int[] ProductID = {2,3,4,5};
-		return ProductID;
-	}
-	
-	public String[] getProductName() {
-		String[] ProductName = {"Chair", "Table", "Gnome" , "Swing"};
-		return ProductName;
-	}
-	
 	
 }
--- src/mwatsonIMS/Product.java
+++ src/mwatsonIMS/Product.java
@@ -2,6 +2,7 @@ package mwatsonIMS;
 
 public class Product {
 
+	//Product Attributes and Stock Quantity 
 	private int ProductID;
 	private int ProductQty;
 	private String ProductName;
@@ -9,33 +10,50 @@ public class Product {
 	public Product() {	
 		
 	}
+	
+	public Product(int ID, int Qty, String Name) {	
+		ProductID = ID;
+		ProductQty = Qty;
+		ProductName = Name;
+	}
+	
+	public Product(int ID) { 
+		ProductID = ID;
+	}
 		
+	//Retrieves Product ID  
 	public int getProductID(){
-		return this.ProductID;
+		return ProductID;
 	}
 	
+	//Retrieves Product Quantity
 	public int getProductQty(){
-		return this.ProductQty;
+		return ProductQty;
 	}
 	
+	//Retrieves Product Name 
 	public String getProductName(){
-		return this.ProductName;
+		return ProductName;
 	}
 	
+	//Setting Product ID 
 	public void setProductID(int ProductID){
-		this.ProductID = ProductID; 
-	
+		this.ProductID = ProductID;
 	}
 	
-	public void setProductQty(int ProductQty){
-		this.ProductQty = ProductQty; 
+	//Setting Product Quantity
+	public void setProductQty(int ProductQty) {
+		this.ProductQty = ProductQty;
 	}
 	
+	//Setting Product Name
 	public void setProductName(String ProductName){
 		this.ProductName = ProductName;
 	}
-	
-	
-	
-	
+
+	@Override
+	public String toString() {
+		return "Product [ProductID=" + ProductID + ", ProductQty=" + ProductQty
+				+ ", ProductName=" + ProductName + "]";
+	}	
 }
